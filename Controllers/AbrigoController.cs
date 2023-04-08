3fd1df7440cc795ac4dfc89a0a222434ecd0d9a5
Wesley Medeiros <90868789+eiwesley@users.noreply.github.com>
2023-04-08 16:09:40 -0300

    :sparkles: feat: Metodo GET geral e GET_ID

--- Controllers/AbrigoController.cs
+++ Controllers/AbrigoController.cs
@@ -32,7 +32,7 @@ public class AbrigoController : Controller
     /// <summary>
     /// Adiciona um tutor ao banco de dados
     /// </summary>
-    /// <param name="petDto">Objeto com os campos necessários para criação de um tutor</param>
+    /// <param name="abrigoDto">Objeto com os campos necessários para criação de um tutor</param>
     /// <returns>IActionResult</returns>
     /// <response code="201">Caso inserção seja feita com sucesso</response>
     [HttpPost]
@@ -42,9 +42,42 @@ public class AbrigoController : Controller
         _context.Abrigo.Add(abrigo);
         _context.SaveChanges();
 
-        return Ok();
+        return CreatedAtAction(nameof(BuscarAbrigoPorId), new { id = abrigo.Id }, abrigo);
     }
 
- 
+    /// <summary>
+    /// Retorna os tutores cadastrados
+    /// </summary>
+    /// <param name="skip">Valor para iniciar a lista</param>
+    /// <param name="take">Valor para trazer a quantidade de tutores</param>
+    /// <returns>IEnumerable</returns>
+    [HttpGet]
+    public IEnumerable<ReadAbrigoDto> BuscarAbrigos([FromQuery] int skip = 0, [FromQuery] int take = 50)
+    {
+        return _mapper.Map<List<ReadAbrigoDto>>(_context.Abrigo.Skip(skip).Take(take));
+    }
+
+    /// <summary>
+    /// Retorna um tutor cadastrado
+    /// </summary>
+    /// <param name="id">ID do Pet cadastrado no banco</param>
+    /// <returns>IActionResult</returns>
+    /// <response code="200">Caso a busca seja feita com sucesso</response>
+    /// /// <response code="404">Caso não exista o ID cadastrado</response>
+    [HttpGet("{id}")]
+    public IActionResult BuscarAbrigoPorId(int id)
+    {
+        var abrigo = _context.Abrigo.FirstOrDefault(abrigo => abrigo.Id == id);
+        if (abrigo == null)
+        {
+            return NotFound();
+        }
+
+        var abrigoDto = _mapper.Map<ReadAbrigoDto>(abrigo);
+
+        return Ok(abrigoDto);
+    }
+
+
 }
 
